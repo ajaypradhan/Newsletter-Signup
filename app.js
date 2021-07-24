0ddef3da95686e7ca894ebd5bf20ed69ab946f06
Ajay Pradhan <pradhanajay14@gmail.com>
2021-07-24 17:09:10 +0530

    all done

--- app.js
+++ app.js
@@ -35,17 +35,22 @@ app.post('/', function (req, res) {
         auth: 'ajay:2109aa00372dd5c5d4b456a6f9f2d289-us17',
     };
     const request = https.request(url, options, function (response) {
+        if (response.statusCode == 200) {
+            res.sendFile(__dirname + '/success.html');
+        } else {
+            res.sendFile(__dirname + '/failure.html');
+        }
         response.on('data', function (data) {
             console.log(JSON.parse(data));
         });
     });
 
-    request.write(jsonData);de
+    request.write(jsonData);
     request.end();
 });
 
-app.listen(3000, function () {
-    console.log('Server is running on port 3000');
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Server is running on port ');
 });
 
 // API KEY
